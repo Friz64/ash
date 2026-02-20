f5ed6ed50c2c7569941c9c73199115d8d4ed8403
Friz64 <friz64@protonmail.com>
2026-02-20 19:57:38 +0100

    ?

--- analysis/src/xml/cexpr.rs
+++ analysis/src/xml/cexpr.rs
@@ -1,1 +1,18 @@
+pub enum CExpr<'a> {
+    Punct(&'a str),
+    Value(&'a str),
+    Cast {
+        to_type: &'a str,
+        expr: Box<CExpr<'a>>,
+    },
+    MacroCall {
+        macro_name: &'a str,
+        args: Vec<CExpr<'a>>,
+    },
+}
 
+impl<'a> CExpr<'a> {
+    pub(crate) fn parse_into(s: &'a str, out: &mut impl Extend<CExpr<'a>>) {
+        // whatevers
+    }
+}
