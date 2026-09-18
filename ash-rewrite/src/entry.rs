30bc06692e90fbf4135a79db5080b9c324b74be3
Friz64 <friz64@protonmail.com>
2026-09-18 21:20:07 +0200

    fix nostd compilation

--- ash-rewrite/src/entry.rs
+++ ash-rewrite/src/entry.rs
@@ -240,6 +240,7 @@ impl Entry {
         }
     }
 
+    #[allow(rustdoc::broken_intra_doc_links)] // "linked" feature might be disabled
     /// <https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkCreateInstance.html>
     ///
     /// # Safety
